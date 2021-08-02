f44279fbede6bd138d11e3eee5c4fa3850e22c2e
kiwifirst <87682994+kiwifirst@users.noreply.github.com>
2021-08-02 23:20:31 +0100

    add basic etch a sketch function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createSquares(size) {
 
     const squareContainer = document.querySelector('.squareContainer');
     squareContainer.style.cssText = `grid-template-columns: repeat(${size}, ${divWidth}px);`;
+    squareContainer.textContent = '';
 
     for (let i = 0; i < size * size; i++) {
         const squareDiv = document.createElement('div');
@@ -13,4 +14,23 @@ function createSquares(size) {
     }
 }
 
-createSquares(10);
+function addSquareListener() {
+    const squares = document.querySelectorAll('.squareDiv');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.classList.add('colored');
+        });
+    });
+}
+
+function promptMessage() {
+    const userSize = prompt('Enter your choice of grid size', '');
+    createSquares(userSize);
+    addSquareListener()
+}
+
+createSquares(16);
+addSquareListener()
+
+const clearButton = document.querySelector('#clearButton');
+clearButton.addEventListener('click', promptMessage);
